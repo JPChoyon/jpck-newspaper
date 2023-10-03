5834de0facfce54cd3208eb3d97b72248b4101ba
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-10-03 12:34:09 +0600

    done left side bar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import Navbar from "../../Component/Navbar/Navbar";
 import Header from "../Shared/Header/Header";
+import RightItems from "../Shared/RightItems/RightItems";
 import BreakingNews from "./BreakingNews";
+
 
 const Home = () => {
   return (
@@ -10,14 +12,10 @@ const Home = () => {
       <Navbar></Navbar>
       <div className="total grid grid-cols-1 md:grid-cols-4 md:gap-5">
         <div className="leftCon ">
-left
+         
         </div>
-        <div className="middleCon col-span-2  ">
-middle
-        </div>
-        <div className="rightCon ">
-right
-        </div>
+        <div className="middleCon col-span-2  ">middle</div>
+        <div className="rightCon "><RightItems></RightItems></div>
       </div>
     </div>
   );
--- src/Pages/Shared/RightItems/RightItems.jsx
+++ src/Pages/Shared/RightItems/RightItems.jsx
@@ -1,9 +1,59 @@
-
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
+import qzone1 from '../../../assets/qZone1.png'
+import qzone2 from '../../../assets/qZone2.png'
+import qzone3 from '../../../assets/qZone3.png'
 
 const RightItems = () => {
   return (
-    <div>
-      this is right items
+    <div className="px-4">
+      {/* Log in  */}
+      <div className="mb-7">
+        <h2 className="text-xl font-semibold">Log In</h2>
+        <button className="btn btn-outline btn-accent w-full">
+          <FaGoogle></FaGoogle>
+          Login with Google
+        </button>
+        <button className="btn btn-outline btn-accent w-full mt-2">
+          <FaGithub></FaGithub>
+          Login with Github
+        </button>
+      </div>
+
+      {/* Follow Us */}
+      <div className="mb-7">
+        <h2 className="text-xl font-semibold">Find Us On</h2>
+        <div className="flex items-center gap-3 border-2 rounded-t-xl py-4 text-xl px-2">
+          <FaFacebook></FaFacebook>
+          <a href="https://www.facebook.com/jpchoyon.khan.130/">Facebook</a>
+        </div>
+        <div className="flex items-center gap-3 border-x-2  py-4 text-xl px-2">
+          <FaTwitter></FaTwitter>
+          <a href="https://twitter.com/JPChoyonKhan1">Twitter</a>
+        </div>
+        <div className="flex items-center gap-3 border-2 rounded-b-xl py-4 text-xl px-2">
+          <FaInstagram></FaInstagram>
+          <a href="https://www.instagram.com/jpchoyonkhan3/">Instagram</a>
+        </div>
+      </div>
+
+      {/* Q-zone */}
+
+      <div>
+        <h2 className="text-xl font-semibold">Q-Zone</h2>
+        <div>
+          <img src={qzone1} alt="" />
+          
+        </div>
+        <div>
+          <img src={qzone2} alt="" />
+
+        </div>
+        <div>
+          <img src={qzone3} alt="" />
+
+        </div>
+
+      </div>
     </div>
   );
 };
